1d10e5aceeb5a7df5c48ec70f70217aa54bda25a
Taurai <20655837+Tauraih@users.noreply.github.com>
2020-10-24 23:13:58 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -64,7 +64,7 @@ def reset_password(token):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
